02af94bb0062e415fc5dee856d11139a50f1903b
jasminepeng8 <59179740+jasminepeng8@users.noreply.github.com>
2019-12-28 19:02:19 -0800

    Create thread.h

---
+++ Project 4/thread.h
@@ -0,0 +1,15 @@
+
+void printHistogram(int *hist, int n);
+
+void *histo_0(void *vargp);
+
+void *histo_1(void *vargp);
+
+void *histo_2(void *vargp);
+
+void *histo_3(void *vargp);
+
+void *histo_4(void *vargp);
+
+void *histo_5(void *vargp);
+
